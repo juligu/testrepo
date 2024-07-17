20326e0c2afa2c2e9a425a709c66c7f212e90f53
Julio Guzman <juligu@microsoft.com>
2024-07-17 11:34:26 -0400

    OPenAPI

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b2be990e2c3cd953d979b7010d67356345f9034")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7131fcc84ffcd740818bc269fef85f633a2ddaff")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
