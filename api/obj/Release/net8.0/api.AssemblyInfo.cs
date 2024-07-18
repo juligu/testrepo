6c0abe592e143c78bb47988a7b7e885b1618ab62
Julio Guzman <juligu@microsoft.com>
2024-07-18 09:17:30 -0400

    Add

--- api/obj/Release/net8.0/api.AssemblyInfo.cs
+++ api/obj/Release/net8.0/api.AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7cd6cbb41314f72ee5fa8fc7cb489fd464a51ad")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
